5f1cedf69b93dfcb857c24ccf90ed4d00a494e68
notconfusing <isalix@gmail.com>
2014-06-13 14:03:18 -0700

    successful uploads to commons, harmonizing filenames, and embedding in docs

---
+++ commons_template.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def make_datestring(year, month, day):
+    datestring = "%04d" % year  # YYYY
+    if month is not None:
+        datestring += "-%02d" % month  # YYYY-MM
+    if day is not None:
+        datestring += "-%02d" % day  # YYYY-MM-DD
+    return datestring
+
+
+def _escape(text):
+    for original, replacement in [
+        ('=', '{{=}}'),
+        ('|', '{{!}}')
+    ]:
+        try:
+            text = text.replace(original, replacement)
+        except AttributeError:
+            pass
+    return text
+
+def _trim(text):
+    return ' '.join(text.split())
+
+def page(metadata):
+    
+    article_doi = metadata['doi']
+    authors = metadata['article-contrib-authors']
+    article_title = metadata['article-title'] 
+    # = metadata['article-abstract']
+    journal_title = metadata['journal-title']
+    article_year = metadata['article-year']
+    article_month = metadata['article-month']
+    article_day = metadata['article-day']
+    article_url = metadata['article-url']
+    license_url = metadata['article-license-url']
+    # = metadata['article-license-text']
+    # = metadata['article-copyright-statement']
+    # = metadata['article-copyright-holder']
+    categories = metadata['article-categories']
+
+    mimetype = 'image'
+    
+    
+    license_templates = {
+        u'http://creativecommons.org/licenses/by/2.0/': '{{cc-by-2.0}}',
+        u'http://creativecommons.org/licenses/by-sa/2.0/': '{{cc-by-sa-2.0}}',
+        u'http://creativecommons.org/licenses/by/2.5/': '{{cc-by-2.5}}',
+        u'http://creativecommons.org/licenses/by-sa/2.5/': '{{cc-by-sa-2.5}}',
+        u'http://creativecommons.org/licenses/by/3.0/': '{{cc-by-3.0}}',
+        u'http://creativecommons.org/licenses/by-sa/3.0/': '{{cc-by-sa-3.0}}',
+        u'http://creativecommons.org/licenses/by/4.0/': '{{cc-by-4.0}}',
+        u'http://creativecommons.org/licenses/by-sa/4.0/': '{{cc-by-sa-4.0}}'
+    }
+    
+    if license_url:
+        license_template = license_templates[license_url]
+    else:
+        license_template = ''
+
+    text = "=={{int:filedesc}}==\n\n"
+    text += "{{Information\n"
+    
+    description = "%s %s %s" % (_escape('Media belonging to article '), _escape(article_doi), _escape(' which is cited on Wikipedia, and automatically imported.'))
+    
+    text += "|Description=\n"
+    if len(description.strip()) > 0:
+        text+= "{{en|1=%s}}\n" % description
+    text += "|Date= %s\n" % make_datestring(article_year, article_month, article_day)
+    
+
+    label = ("%s file" % mimetype).capitalize()
+    text += "|Source= [%s %s] from " % (article_url, _escape(label))
+    text += "{{Cite journal\n"
+    text += "| author = %s\n" % _escape(authors)
+    text += "| title = %s\n" % _escape(_trim(article_title))
+    text += "| doi = %s\n" % _escape(article_doi)
+    text += "| journal = %s\n" % _escape(journal_title)
+    text += "| year = %s\n" % _escape(article_year)
+    text += "}}\n"
+    text += "|Author= %s\n" % _escape(authors)
+    text += "|Permission= %s\n" % license_template
+    text += "|Other_fields={{Information field|name=Provenance|value= {{Open Access Media Importer}} }}\n"
+    text += "}}\n\n"
+
+    def _capitalize_properly(word):
+        if len(word) == 1: # single letters should pass through unchanged
+            return word
+        if word[1:] == word[1:].lower():  # word has no capital letters inside
+            return word.lower()
+        else:  # words like 'DNA' or 'HeLa' should not be touched
+            return word
+
+    def _postprocess_category(category):
+        if '(' in category:
+            category = category.split('(')[0]
+        if ',' in category:
+            category_parts = category.split(',')
+            category_parts.reverse()
+            category = ' '.join(category_parts)
+        processed_category = []
+        for word in category.strip().split(' '):
+            wordparts = []
+            for part in word.split('-'):
+                wordparts.append(_capitalize_properly(part))
+            processed_category.append('-'.join(wordparts))
+        category = ' '.join(processed_category)
+        return category[0].capitalize() + category[1:]
+
+    for category in categories:
+        category = _postprocess_category(category)
+        if len(category.split()) > 1:  # no single-word categories
+            text += "[[Category:%s]]\n" % _escape(category)
+    text += "[[Category:Media from %s]]\n" % _escape(journal_title)
+    text += "[[Category:Uploaded with Open Access Media Importer]]\n"
+    text += '[[Category:Uploaded with Open Access Media Importer and needing category review]]\n'
+    if mimetype == 'audio':
+        text += '[[Category:Audio files from open-access scholarly articles]]'
+    if mimetype == 'video':
+        text += '[[Category:Videos from open-access scholarly articles]]'
+    return text
--- journal_article.py
+++ journal_article.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import requests
 from bs4 import BeautifulSoup
 import wget
@@ -9,17 +11,21 @@ import xml.etree.ElementTree as etree
 import pywikibot
 from collections import defaultdict
 from functools import wraps
+import re
+import ast
+import pmc_extractor
+import commons_template
 
 class journal_article():
     '''This class represents a journal article 
     and its lifecycle to make it to Wikisource.'''
-    def __init__(self, doi, dirs):
+    def __init__(self, doi, static_vars):
         '''journal_articles are represented by dois'''
         if doi.startswith('http://dx.doi.org/'):
             doi_parts = doi.split('http://dx.doi.org/')
             doi = doi_parts[1] 
         self.doi = doi
-        self.dirs = dirs
+        self.static_vars = static_vars
         #a phase is like, have we downloaded it, have we gotten the pmcid, uploaded the images etc.
         self.phase = defaultdict(bool)
     
@@ -50,7 +56,7 @@ class journal_article():
             archivefile_url = record.oa.records.record.find(format='tgz')['href']
 
             archivefile_name = wget.filename_from_url(archivefile_url)
-            complete_path_targz = os.path.join(self.dirs["data_dir"], archivefile_name)
+            complete_path_targz = os.path.join(self.static_vars["data_dir"], archivefile_name)
             urllib.urlretrieve(archivefile_url, complete_path_targz)
             self.complete_path_targz = complete_path_targz
 
@@ -65,9 +71,9 @@ class journal_article():
     def extract_targz(self):
         try:
             directory_name, file_extension = self.complete_path_targz.split('.tar.gz')
-            self.dirs["article_dir"] = directory_name
+            self.article_dir = directory_name
             tar = tarfile.open(self.complete_path_targz, 'r:gz')
-            tar.extractall(self.dirs["data_dir"])
+            tar.extractall(self.static_vars["data_dir"])
             self.phase['extract_targz'] = True
         except:
             raise ConversionError(message='trouble extracting the targz', doi=self.doi)
@@ -75,33 +81,36 @@ class journal_article():
 
     def find_nxml(self):
         try:
-            self.dirs["qualified_article_dir"] = os.path.join(self.dirs["data_dir"], self.dirs["article_dir"])
-            nxml_files = [file for file in os.listdir(self.dirs["qualified_article_dir"]) if file.endswith(".nxml")]
+            self.qualified_article_dir = os.path.join(self.static_vars["data_dir"], self.article_dir)
+            nxml_files = [file for file in os.listdir(self.qualified_article_dir) if file.endswith(".nxml")]
             if len(nxml_files) != 1:
                 raise ConversionError(message='we need excatly 1 nxml file, no more, no less', doi=self.doi)
             nxml_file = nxml_files[0]
-            self.nxml_path = os.path.join(self.dirs["qualified_article_dir"], nxml_file)
+            self.nxml_path = os.path.join(self.qualified_article_dir, nxml_file)
             self.phase['find_nxml'] = True
         except ConversionError as ce:
             raise ce
         except:
             raise ConversionError(message='could not traverse the search dierctory for nxml files', doi=self.doi)
+        
+    def extract_metadata(self):
+        self.metadata = pmc_extractor.extract_metadata(self.nxml_path)
+        self.phase['extract_metadata'] = True
 
     
     def xslt_it(self):
         try:
             doi_file_name = self.doi + '.mw.xml'
-            mw_xml_file = os.path.join(self.dirs["data_dir"], doi_file_name)
+            mw_xml_file = os.path.join(self.static_vars["data_dir"], doi_file_name)
             doi_file_name_pre_slash = doi_file_name.split('/')[0]
             if doi_file_name_pre_slash == doi_file_name:
                 raise ConversionError(message='i think there should be a slash in the doi', doi=self.doi)
-            mw_xml_dir = os.path.join(self.dirs["data_dir"], doi_file_name_pre_slash)
+            mw_xml_dir = os.path.join(self.static_vars["data_dir"], doi_file_name_pre_slash)
             if not os.path.exists(mw_xml_dir):
                 os.makedirs(mw_xml_dir)
             mw_xml_file_handle = open(mw_xml_file, 'w')
-            call_return = call(['xsltproc', self.dirs["jats2mw_xsl"], self.nxml_path], stdout=mw_xml_file_handle)
+            call_return = call(['xsltproc', self.static_vars["jats2mw_xsl"], self.nxml_path], stdout=mw_xml_file_handle)
             if call_return == 0: #things went well
-                print 'success xslting'
                 mw_xml_file_handle.close()
                 self.mw_xml_file = mw_xml_file
                 self.phase['xslt_it'] = True
@@ -127,31 +136,106 @@ class journal_article():
             root = tree.getroot()
             mwtitle = root.find('mw:page/mw:title', namespaces={'mw':'http://www.mediawiki.org/xml/export-0.8/'})
             self.title = mwtitle.text
+            self.metadata['title'] = mwtitle.text
             self.phase['get_mw_title_element'] = True
         except:
             raise ConversionError(message='mw_title_element not found')
+        
+
 
     def upload_images(self):
-        try:
-            pass
-            self.phase['upload_images'] = True
-        except:
-            raise ConversionError(message='could not upload images')
+        #want to make the name commons-compatible in the way that OAMI does
+        def harmonizing_name(image_name, article_title):
+            '''Copy Pasta-ed from open access media importer to get it the same'''
+            dirty_prefix = article_title
+            dirty_prefix = dirty_prefix.replace('\n', '')
+            dirty_prefix = ' '.join(dirty_prefix.split()) # remove multiple spaces
+            forbidden_chars = u"""?,;:^/!<>"`'±#[]|{}ʻʾʿ᾿῾‘’“”"""
+            for character in forbidden_chars:
+                dirty_prefix = dirty_prefix.replace(character, '')
+            # prefix is first hundred chars of title sans forbidden characters
+            prefix = '-'.join(dirty_prefix[:100].split(' '))
+            # if original title is longer than cleaned up title, remove last word
+            if len(dirty_prefix) > len(prefix):
+                prefix = '-'.join(prefix.split('-')[:-1])
+            if prefix[-1] != '-':
+                prefix += '-'
+            return prefix + image_name
+        
+        #get all the images in our folder with valid extension
+        self.image_files = [image_file for image_file in os.listdir(self.qualified_article_dir) if \
+               any([image_file.endswith(extension) for extension in self.static_vars['image_extensions'] ] ) ]
+        
+        commons = pywikibot.Site('commons', 'commons')
+        if not commons.logged_in():
+            commons.login()
+        #commons = pywikibot.Site('test2', 'wikipedia')
+        
+        #our data type will be a dict with the jpg and what it ended-up being called on commons
+        
+        self.used_image_names = dict()
+
+        for image_file in self.image_files:
+            qualified_image_location = os.path.join(self.qualified_article_dir, image_file)
+            harmonized_name = harmonizing_name(image_file, self.title)
+            #print harmonized_name
+            image_page = pywikibot.ImagePage(commons, harmonized_name)
+            page_text = commons_template.page(self.metadata)
+            image_page._text = page_text
+            try:
+                commons.upload(imagepage=image_page, source_filename=qualified_image_location, comment='Automatic upload of media from: [[doi:' + self.doi+']]')
+                self.used_image_names[image_file] = harmonized_name
+            except pywikibot.exceptions.UploadWarning as warning:
+                warning_string = unicode(warning)
+                if warning_string.startswith('Uploaded file is a duplicate of '):
+                    liststring = warning_string.split('Uploaded file is a duplicate of ')[1][:-1]
+                    duplicate_list = ast.literal_eval(liststring)
+                    duplicate_name = duplicate_list[0]
+                    print 'duplicate found: ', duplicate_name
+                    self.used_image_names[image_file] = duplicate_name
+                elif warning_string.endswith('already exists.'):
+                    self.used_image_names[image_file] = harmonized_name
+                else:
+                    raise
+        self.phase['upload_images'] = True
+
+                
+    def replace_image_names_in_wikitext(self):
+        
+        image_names_without_extension = {image_file: os.path.splitext(image_file)[0] for image_file in self.used_image_names.iterkeys()}
+        
+        replacing_text = self.wikitext
+        for image_file, image_name_without_extension in image_names_without_extension.iteritems():
+            extensionless_re = r'File:(' + image_name_without_extension + r')\|'
+            new_file_text = r'File:' + self.used_image_names[image_file] + r'|'
+            replacing_text, occurences = re.subn(extensionless_re, new_file_text, replacing_text)
+            if occurences < 1:
+                print occurences, image_file, image_name_without_extension
+            if occurences > 1:
+                # see if it has the .jpg already baked i
+                print occurences, image_file, image_name_without_extension
+        #print replacing_text
+        self.image_fixed_wikitext = replacing_text
+
+        self.phase['replace_image_names_in_wikitext'] = True
+                        
 
     def push_to_wikisource(self):
-        site = pywikibot.Site(self.dirs["wikisource_site"], "wikisource")
-        page = pywikibot.Page(site, self.dirs["wikisource_basepath"] + self.title)
+        site = pywikibot.Site(self.static_vars["wikisource_site"], "wikisource")
+        #site = pywikibot.Site('test2', "wikipedia")
+        page = pywikibot.Page(site, self.static_vars["wikisource_basepath"] + self.title)
+        #page = pywikibot.Page(site, 'Wikipedia:DOIUpload/' + self.title)
         comment = "Imported from [[doi:"+self.doi+"]] by recitationbot"
-        page.put(newtext=self.wikitext, botflag=True, comment=comment)
+        page.put(newtext=self.image_fixed_wikitext, botflag=True, comment=comment)
         self.wiki_link = page.title(asLink=True)
         self.phase['push_to_wikisource'] = True
     
 
     def push_redirect_wikisource(self):
-        site = pywikibot.Site(self.dirs["wikisource_site"], "wikisource")
-        page = pywikibot.Page(site, self.dirs["wikisource_basepath"] + self.doi)
+        site = pywikibot.Site(self.static_vars["wikisource_site"], "wikisource")
+        page = pywikibot.Page(site, self.static_vars["wikisource_basepath"] + self.doi)
         comment = "Making a redirect"
-        redirtext = '#REDIRECT [[' + self.dirs["wikisource_basepath"] + self.title +']]'
+        redirtext = '#REDIRECT [[' + self.static_vars["wikisource_basepath"] + self.title +']]'
         page.put(newtext=redirtext, botflag=True, comment=comment)
         self.phase['push_redirect_wikisource'] = True
 
