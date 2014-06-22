e68e7b5d98c73f3bf4aa1232c6357a0b3e62dd7b
notconfusing <isalix@gmail.com>
2014-06-22 06:49:10 +0000

    putting the user-config file up per request

---
+++ user-config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8  -*-
+family = 'wikipedia'
+mylang = 'en'
+usernames['wikipedia']['en'] = u'recitation-bot'
+usernames['wikisource']['en'] = u'recitation-bot'
+usernames['commons']['commons'] = u'recitation-bot'
