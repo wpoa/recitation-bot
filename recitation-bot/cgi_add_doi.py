31a26d5e088b3b20ab63ca2a32c499fd73113c08
notconfusing <isalix@gmail.com>
2014-06-23 00:53:30 +0000

    made the cgi app

---
+++ recitation-bot/cgi_add_doi.py
@@ -0,0 +1,42 @@
+import cgi
+import cgitb
+
+cgitb.enable()
+
+print "Content-Type: text/html"     # HTML is following
+print                               # blank line, end of headers
+print "<TITLE>jump a doi to the front of the queue</TITLE>"
+print "<H1>jump a doi to the front of the queue</H1>"
+#print "who dares enter a doi"
+print "<html>"
+print "<body>"
+
+# standard single line text field
+print "<form method='post'>"
+print "Enter a doi (don't include 'http://dx.doi.org/'):"
+print "<input type='text' name='doi' value='' />"
+print "<input type='submit' value='submit form' />"
+print "</form>"
+form = cgi.FieldStorage()
+#print form
+print "<h2>I will try and upload:</h2>"
+print "<ul>"
+for field_name in form:
+    field=form[field_name]
+    if field.name == 'doi':
+        print "<li>"
+        print field.name
+        print " = "
+        doi = cgi.escape(repr(field.value))
+        print doi
+        print "</li>"
+        if form['doi'].value:
+            dequeue = open('/data/project/recitation-bot/recitation-bot/jump_the_queue.log','a')
+            dequeue.write(doi+'\n')
+            dequeue.close()
+
+print "</ul>"
+
+
+print "</body>"
+print "</html>"
