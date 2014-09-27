e1ea9550f58b39b886dcb7381c3c858841884676
Daniel Mietchen <daniel.mietchen@googlemail.com>
2014-09-27 03:10:51 +0200

    comment

--- recitation-bot/twython_access.py
+++ recitation-bot/twython_access.py
@@ -15,6 +15,6 @@ def update_status(ja):
     twitterstr += ja.metadata['article-title'][:75] #Hopefully this will keep it undeer 140 characters but it might not if there's lots of unicode
 #    What about trying to determine a hashtag first (default: ' #Biology'), then subtract its length from 86 and shorten the article title to the last word that fits in completely?
 #    twitterstr += ja.metadata['hashtag'] 
-    twitterstr += ja.urlstr()
+    twitterstr += ja.urlstr() #Perhaps better use the DOI-based page title here, so that altmetrics can pick it up
     logging.info(twitterstr)
     twitter.update_status(status=twitterstr)
