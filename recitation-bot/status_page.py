2452bc9d4eee559ed78c1b79228922f2225495a0
notconfusing <isalix@gmail.com>
2014-10-22 00:51:08 +0000

    allows dirs to be made for new log files, based on string '/'s

--- recitation-bot/status_page.py
+++ recitation-bot/status_page.py
@@ -21,12 +21,20 @@ def make_status_page(doi, success, error_msg, ja, inqueue):
         doiurl = None #we actually check for this being empty
     output = status_template.render(doi=doi, success_str=success_str, error_msg=error_msg, metadata=metadata, doiurl=doiurl, time=time, inqueue=inqueue)
 
-    page_path = '/data/project/recitation-bot/public_html/'+doi+'.html'
+    page_base_path = '/data/project/recitation-bot/public_html/'
+    doi_split = doi.rsplit('/', 1) # split on last '/', always at least 1
+    page_sub_path = doi_split[0]
+    page_file_name = doi_split[1]
+    page_dir_path = page_base_path + page_sub_path + "/"
+    page_full_path = page_dir_path + page_file_name + '.html'
+
     old_page_content = ''
-    if os.path.isfile(page_path):
-        old_page_content = open(page_path, 'r').read()
+    if os.path.isfile(page_full_path):
+        old_page_content = open(page_full_path, 'r').read()
+    elif not os.path.exists(page_dir_path):
+        os.makedirs(page_dir_path)
     appended_output = output + old_page_content.decode('utf-8')
-    page = open(page_path, 'w')
+    page = open(page_full_path, 'w')
     uni_out = appended_output.encode('utf-8')
     page.write(uni_out)
     page.close()
